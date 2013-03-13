ae1fcfee8f4dc092676b5dd1854f2be5adaddf0b
Antoine CARON <Slashgear@users.noreply.github.com>
2013-03-13 16:05:34 +0000

    Ajout du .h Mur et Grid

---
+++ tron-lif7/src/Grid.h
@@ -0,0 +1,22 @@
+#ifndef GRID_H_INCLUDED
+#define GRID_H_INCLUDED
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+*\file [Grid.h]
+*\brief Module des vecteurs
+*\author {Antoine.C,Matthieu.B}
+*\version 0.1
+*\date 13 mars 2013
+*/
+
+typedef struct{
+    unsigned int positionX; //positionX de la première case
+    unsigned int positionY; //positionY de la première case
+    unsigned int tailleX;
+    unsigned int tailleY;
+    Mur * tab_terrain;
+}Grid;
+
+#endif // GRID_H_INCLUDED
---
+++ tron-lif7/src/Mur.h
@@ -0,0 +1,25 @@
+#ifndef MUR_H_INCLUDED
+#define MUR_H_INCLUDED
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+*\file [Grid.h]
+*\brief Module des vecteurs
+*\author {Antoine.C,Matthieu.B}
+*\version 0.1
+*\date 13 mars 2013
+*/
+
+typedef struct{
+    unsigned int positionX; //positionX de la première case
+    unsigned int positionY; //positionY de la première case
+    unsigned int tailleX;
+    unsigned int tailleY;
+    Mur * tab_terrain;
+}Mur;
+
+
+
+
+#endif // MUR_H_INCLUDED
