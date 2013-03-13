99be26a74dcce03441dd8e258b69e6cbe3fe26be
antoine395.caron@laposte.net <antoine395.caron@laposte.net@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-13 16:46:26 +0000

    Ajout de quelques fonctions à Vecteur2D

---
+++ trunk/tron-lif7/src/Vecteur2D.c
@@ -0,0 +1,24 @@
+#include "Vecteur2D.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+float norme (Vector2D vecteur){
+    return sqrt((vecteur.X)*(vecteur.X)+(vecteur.Y)*(vecteur.Y));
+}
+
+float getX(Vector2D vecteur){
+    return vecteur.X;
+}
+
+float getY(Vector2D vecteur){
+    return vecteur.Y;
+}
+
+void setX(Vector2D vecteur,float x){
+    vecteur.X=x;
+}
+
+void setY(Vector2D vecteur,float y){
+    vecteur.Y=y;
+}
