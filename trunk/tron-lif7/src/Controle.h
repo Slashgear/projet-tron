1dc642fb898ea1dff0cdab6c6bb915f89978de58
antoine395.caron@laposte.net <antoine395.caron@laposte.net@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-19 11:53:05 +0000

    Ajout de controle

---
+++ trunk/tron-lif7/src/Controle.h
@@ -0,0 +1,47 @@
+#ifndef CONTROLE_H_INCLUDED
+#define CONTROLE_H_INCLUDED
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+*\file [Controle.h]
+*\brief Module des vecteurs
+*\author {Antoine.C,Matthieu.B}
+*\version 0.1
+*\date 13 mars 2013
+*/
+
+typedef struct{
+    char droite;
+    char haut;
+    char bas;
+    char gauche;
+}Controle;
+
+/** assesseur de touche droite*/
+char ControleGetDroite(const Controle* controle);
+/** assesseur de touche haut*/
+char ControlGetHaut(const Controle* controle);
+/** assesseur de touche bas*/
+char ControlGetBas(const Controle* controle);
+/** assesseur de touche gauche*/
+char ControlGetGauche(const Controle* controle);
+
+/** mutateur de touche droite*/
+void ControleSetDroite(const Controle* controle,char x);
+/** mutateur de touche haut*/
+void ControleSetHaut(const Controle* controle,char x);
+/** mutateur de touche bas*/
+void ControleSetBas(const Controle* controle,char x);
+/**mutateur de touche gauche*/
+void ControleSetGauche(const Controle* controle,char x);
+
+/** Constructeur de Controle*/
+void ControleConstructeur(Controle* controle);
+/** Destructeur de Controle*/
+void ControleDestructeur(Controle* controle);
+
+/** Procedure qui teste le module Controle*/
+ void ControleTestRegression();
+
+#endif /* CONTROLE_H_INCLUDED */
