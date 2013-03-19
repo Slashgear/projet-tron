65f2982dab8c6a705733555505f35816cbcab325
Antoine CARON <Slashgear@users.noreply.github.com>
2013-03-19 16:14:02 +0000

    rectification de l'erreur liée au ; de Mur.h

--- tron-lif7/src/Grid.c
+++ tron-lif7/src/Grid.c
@@ -4,40 +4,5 @@
 #include "Mur.h"
 
 
-float GridGetPositionX(Grid g){
-    return g.positionX;
-}
 
-float GridGetPositionY(Grid g){
-    return g.positionY;
-}
-
-unsigned int GridGetTailleX(Grid g){
-    return g.tailleX;
-}
-
-unsigned int GridGetTailleY(Grid g){
-    return g.tailleY;
-}
-
-void GridSetPositionX(Grid g,float p){
-    //assert(p>=0);
-    g.positionX=p;
-}
-void GridSetPositionY(Grid g,float p){
-    //assert(p>=0);
-    g.positionX=p;
-}
-void GridSetTailleX(Grid g,unsigned int t){
-   // assert(t>=0);
-    g.tailleX=t;
-}
-void GridSetTailleY(Grid g,unsigned int t){
-   // assert(t>=0);
-    g.tailleY=t;
-}
-void GridSetMesMurs(Grid g,Mur* mur){
-   //assert(mur!=NULL);
-    g.mesMurs=mur;
-}
 
