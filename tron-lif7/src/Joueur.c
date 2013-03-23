1b06032e282baa91c5c44e69dbd1c9ad7de603b6
matthieu.billet3 <matthieu.billet3@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-23 19:33:09 +0000

    Joueur.c fonctionne

--- tron-lif7/src/Joueur.c
+++ tron-lif7/src/Joueur.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include "Joueur.h"
 #include "Moto.h"
+#include "Controle.h"
 #include "Couleur.h"
 
 
@@ -15,28 +16,66 @@ Couleur JoueurGetCouleur(const Joueur* joueur){
     return joueur->couleur;
 }
 
-void JoueurSetMoto(Joueur* joueur, Moto moto){
-    joueur->moto=&moto;
+void JoueurSetMoto(Joueur* joueur, Moto* moto){
+    joueur->moto=moto;
 }
-void JoueurSetControle(Joueur* joueur, Controle controle){
-    joueur->controle=&controle;
+void JoueurSetControle(Joueur* joueur, Controle* controle){
+    joueur->controle=controle;
 }
 void JoueurSetCouleur(Joueur* joueur, Couleur couleur){
     joueur->couleur=couleur;
 }
 
-/** Constructeur de Joueur*/
-void JoueurConstructeur(Joueur* joueur,const Moto* moto,const Controle* controle, Couleur couleur){
-    JoueurSetMoto(joueur,*moto);
-    JoueurSetControle(joueur,*controle);
+void JoueurConstructeur(Joueur* joueur, Moto* moto, Controle* controle, Couleur couleur){
+    JoueurSetMoto(joueur,moto);
+    JoueurSetControle(joueur,controle);
     JoueurSetCouleur(joueur,couleur);
 }
 /** Destructeur de Joueur*/
 void JoueurDestructeur(Joueur* joueur){
-
+    MotoDestructeur(joueur->moto);
+    ControleDestructeur(joueur->controle);
+    JoueurSetMoto(joueur,NULL);
+    JoueurSetControle(joueur,NULL);
+    JoueurSetCouleur(joueur,NOIR);
 }
 
 /** Procedure qui teste le module Joueur*/
  void JoueurTestRegression(){
+    Joueur joueur;
+    Moto moto;
+    Controle controle;
+    Couleur couleur=ORANGE;
+    float posX=1;
+    float posY=1;
+    unsigned int tailleX=10;
+    unsigned int tailleY=10;
+    float vitesse=20;
+    Direction direction=HAUT;
+    MotoConstructeur(&moto,posX,posY,tailleX,tailleY,vitesse,direction);
+
+    char touchedroite='d';
+    char touchegauche='g';
+    char touchehaut='h';
+    char touchebas='b';
+    ControleConstructeur(&controle,touchedroite,touchegauche,touchehaut,touchebas);
+
+    JoueurConstructeur(&joueur,&moto,&controle,couleur);
+    printf("La valeur posX est %f et dans la Moto du joueur est de %f \n",posX,MotoGetPositionX(JoueurGetMoto(&joueur)));
+    printf("La valeur posY est %f et dans la Moto du joueur est de %f \n",posY,MotoGetPositionY(JoueurGetMoto(&joueur)));
+    printf("La valeur tailleX est %u et dans la Moto du joueur est de %u \n",tailleX,MotoGetTailleX(JoueurGetMoto(&joueur)));
+    printf("La valeur tailleY est %u et dans la Moto du joueur est de %u \n",tailleY,MotoGetTailleY(JoueurGetMoto(&joueur)));
+    printf("La valeur vitesse est %f et dans la Moto du joueur est de %f \n",vitesse,MotoGetVitesse(JoueurGetMoto(&joueur)));
+    printf("La valeur de direction est de %d et dans la Moto du joueur est de %d \n",direction,MotoGetDirection(JoueurGetMoto(&joueur)));
+
+    printf("Le caractere de la touche droite est %c ,et dans le Controle du joueur est %c\n",touchedroite,ControleGetDroite(JoueurGetControle(&joueur)));
+    printf("Le caractere de la touche gauche est %c ,et dans le Controle du joueur est %c\n",touchegauche,ControleGetGauche(JoueurGetControle(&joueur)));
+    printf("Le caractere de la touche haut est %c ,et dans le Controle du joueur est %c\n",touchehaut,ControleGetHaut(JoueurGetControle(&joueur)));
+    printf("Le caractere de la touche bas est %c ,et dans le Controle du joueur est %c\n",touchebas,ControleGetBas(JoueurGetControle(&joueur)));
+
+    JoueurDestructeur(&joueur);
+
+    printf("Après destruction le pointeur de moto est %p \n", JoueurGetMoto(&joueur));
+    printf("Après destruction le pointeur de controle est %p \n \n", JoueurGetControle(&joueur));
 
  }
