b3a887d6db6f77dd87c6dcc4d9fcc75a57c16a2c
matthieu.billet3 <matthieu.billet3@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-23 17:22:31 +0000

    C'est mieux pour mur.h !

--- trunk/tron-lif7/src/Couleur.h
+++ trunk/tron-lif7/src/Couleur.h
@@ -1,6 +1,6 @@
 #ifndef COULEUR_H_INCLUDED
 #define COULEUR_H_INCLUDED
 
-typedef enum{ORANGE=1, BLEU=2} Couleur;
+typedef enum{NOIR=0, ORANGE=1, BLEU=2} Couleur; /*ORANGE et BLEU sont les couleurs des joueurs 1 et 2. NOIR signifie que l'objet n'est pas attribué à un joueur.*/
 
 #endif /* COULEUR_H_INCLUDED */
