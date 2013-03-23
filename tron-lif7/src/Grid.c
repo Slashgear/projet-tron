9df519ef889789efad77e122f642612c403cc9bd
Antoine CARON <Slashgear@users.noreply.github.com>
2013-03-23 18:30:57 +0000

    modification du code de grid.c

--- tron-lif7/src/Grid.c
+++ tron-lif7/src/Grid.c
@@ -2,6 +2,38 @@
 #include<stdio.h>
 #include "Grid.h"
 #include "Mur.h"
+#include "TableauDynamique.h"
+
+float GridGetPositionX(const Grid* grille){
+    return grille->positionX;
+}
+float GridGetPositionY(const Grid* grille){
+    return grille->positionY;
+}
+unsigned int GridGetTailleX(const Grid* grille){
+    return grille->tailleX;
+}
+unsigned int GridGetTailleY(const Grid* grille){
+    return grille->tailleY;
+}
+TableauDynamique* GridGetMesMurs(const Grid* grille){
+    return grille->mesMurs;
+}
+void GridSetPositionX(Grid* grille,float posX){
+    grille->positionX=posX;
+}
+void GridSetPositionY(Grid* grille,float posY){
+    grille->positionY=posY;
+}
+void GridSetTailleX(Grid* grille,unsigned int tailleX){
+    grille->tailleX=tailleX;
+}
+void GridSetTailleY(Grid* grille,unsigned int tailleY){
+    grille->tailleY=tailleY;
+}
+void GridSetMesMurs(Grid* grille,const TableauDynamique* mesMurs){
+    grille->mesMurs=mesMurs;
+}
 
 
 
