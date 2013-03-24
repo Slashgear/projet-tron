6ef33c3afbb1b1cac47b8b413824a743309bfc37
antoine395.caron@laposte.net <antoine395.caron@laposte.net@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-24 13:56:15 +0000

    ajout de grid test regression

--- trunk/tron-lif7/src/main.c
+++ trunk/tron-lif7/src/main.c
@@ -4,11 +4,13 @@
 #include "Moto.h"
 #include "Controle.h"
 #include "Joueur.h"
+#include "Grid.h"
 
 int main(){
     MurTestRegression();
     ControleTestRegression();
     MotoTestRegression();
     JoueurTestRegression();
+    GridTestRegression();
 return 0;
 }
