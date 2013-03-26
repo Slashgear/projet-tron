2191e51ee088905b9ee0fdb2ae3377ab2cbe6f8a
matthieu.billet3 <matthieu.billet3@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-03-26 14:32:17 +0000

    on commence sdl.c

--- tron-lif7/src/SDL.h
+++ tron-lif7/src/SDL.h
@@ -13,75 +13,25 @@
 
 typedef struct{
     Jeu *jeu;
-    SDL_Surface * fond;
-    SDL_Surface * moto1H;
-    SDL_Surface * moto1B;
-    SDL_Surface * moto1G;
-    SDL_Surface * moto1D;
-    SDL_Surface * moto2H;
-    SDL_Surface * moto2B;
-    SDL_Surface * moto2G;
-    SDL_Surface * moto2D;
+    SDL_Surface * textures[9];
 }SDL;
+/** La 0ieme texture est le fond, les textures 1 à 4 sont celles du joueur 1, les quatre suivantes celles du joueur 2, etc..*/
 
 /**assesseur de jeu*/
 Jeu * SDLGetJeu(const SDL* sdl);
-/** assesseur de fond*/
-SDL_Surface * SDLGetFond(const SDL* sdl);
-/** assesseur de moto1H*/
-SDL_Surface * SDLGetMoto1H(const SDL* sdl);
-/** assesseur de moto1B*/
-SDL_Surface * SDLGetMoto1B(const SDL* sdl);
-/**assesseur de moto1G*/
-SDL_Surface * SDLGetMoto1G(const SDL* sdl);
-/**assesseur de moto1D*/
-SDL_Surface * SDLGetMoto1D(const SDL* sdl);
-/** assesseur de moto2H*/
-SDL_Surface * SDLGetMoto2H(const SDL* sdl);
-/** assesseur de moto2B*/
-SDL_Surface * SDLGetMoto2B(const SDL* sdl);
-/**assesseur de moto2G*/
-SDL_Surface * SDLGetMoto2G(const SDL* sdl);
-/**assesseur de moto2D*/
-SDL_Surface * SDLGetMoto2D(const SDL* sdl);
+/** assesseur de ieme texture*/
+SDL_Surface * SDLGetIemeTexture(const SDL* sdl,unsigned int i);
+/** assesseur de textures*/
+SDL_Surface * SDLGetTextures(const SDL* sdl);
+
 
 /**mutateur de jeu*/
 void SDLSetJeu(SDL * sdl,Jeu* jeu);
-/**mutateur de fond*/
-void SDLSetFond(SDL* sdl,SDL_Surface * fond);
-/**mutateur de moto1H*/
-void SDLSetMoto1H(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto1B*/
-void SDLSetMoto1B(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto1G*/
-void SDLSetMoto1G(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto1D*/
-void SDLSetMoto1D(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto2H*/
-void SDLSetMoto2H(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto2B*/
-void SDLSetMoto2B(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto2G*/
-void SDLSetMoto2G(SDL* sdl,SDL_Surface * moto);
-/**mutateur de moto1D*/
-void SDLSetMoto2D(SDL* sdl,SDL_Surface * moto);
+/**mutateur de ieme texture*/
+void SDLSetIemeTexture(SDL *sdl,unsigned int i,SDL_Surface * texture);
 
-/** Constructeur de SDL, initialise les différents champs*/
-void SDLConstructeur(SDL *sdl,Jeu* jeu);
-/**Destructeur de SDl,remise à zero des champs de SDL et free des allocations*/
-void SDLDestructeur(SDL *sdl);
-/** Procédure d'affichage du jeu*/
-void SDLAfficheJeu(SDL *sdl);
-/**Procédure d'affichage de la grille*/
-void SDLAfficheGrid(SDL *sdl);
-/** Boucle d'affichage du Jeu */
-void SDLBoucleJeu(SDL* sdl);
-/** Procédure de chargement des images*/
-SDL_Surface* SDLChargeImage(const char *nomfichier);
-/**Procédure qui applique la surface A sur la B*/
-void SDLAppliqueSurface(SDL_Surface * surfaceA, SDL_Surface * surfaceB,const float positionX,const float positionY);
 
 /** Pocédure qui teste le Module*/
-void SDLTestRegression(SDL* sdl);
+void SDLTestRegression();
 
 #endif /* SDL_H_INCLUDED */
