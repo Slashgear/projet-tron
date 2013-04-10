61892cc6d0be59d43aabd006f73a80d6eb334aef
Antoine CARON <Slashgear@users.noreply.github.com>
2013-04-10 18:53:38 +0000

    Ajout des .h des Bonus

--- tron-lif7/src/Constantes.h
+++ tron-lif7/src/Constantes.h
@@ -2,7 +2,7 @@
 #define CONSTANTES_H_INCLUDED
 
 #define _Nombre_de_Joueur 4
-#define _Duree_Vie_Mur 175
+#define _Duree_Vie_Mur 350/_Nombre_de_Joueur
 #define _Vitesse_Initiale 3
 #define _Acceleration 0.02f
 
---
+++ tron-lif7/src/EffetBonus.h
@@ -0,0 +1,14 @@
+#ifndef EFFETBONUS_H_INCLUDED
+#define EFFETBONUS_H_INCLUDED
+
+/**
+\file [EffetBonus.h]
+\brief Module des Motos du jeu
+\author {Antoine.C,Matthieu.B}
+\version 1.1
+\date 10 avril 2013
+*/
+typedef enum{AUCUN=0,NETTOYAGE=1,BOOST=2}EffetBonus;
+
+
+#endif // EFFETBONUS_H_INCLUDED
