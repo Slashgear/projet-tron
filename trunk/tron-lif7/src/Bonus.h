8c3f81e5440ffb398de7ed7f6eed7a8d0a848a69
antoine395.caron@laposte.net <antoine395.caron@laposte.net@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-04-10 18:53:38 +0000

    Ajout des .h des Bonus

---
+++ trunk/tron-lif7/src/Bonus.h
@@ -0,0 +1,50 @@
+#ifndef BONUS_H_INCLUDED
+#define BONUS_H_INCLUDED
+#include "Jeu.h"
+#include "EffetBonus.h"
+
+/**
+\file [Bonus.h]
+\brief Module des Motos du jeu
+\author {Antoine.C,Matthieu.B}
+\version 1.1
+\date 10 avril 2013
+*/
+
+typedef struct{
+    float positionX;
+    float positionY;
+    unsigned int tailleX;
+    unsigned int tailleY;
+    EffetBonus effet;
+}Bonus;
+
+/**assesseur de Bonus*/
+float BonusGetPositionX(const Bonus bonus);
+float BonusGetPositionX(const Bonus bonus);
+unsigned int BonusGetTailleX(const Bonus bonus);
+unsigned int BonusGetTailleY(const Bonus bonus);
+EffetBonus BonusGetEffet(const Effet effet);
+
+/**mutateur de Bonus*/
+void BonusSetPositionX(Bonus bonus,float positionX);
+void BonusSetPositionX(Bonus bonus,float positionY);
+void BonusSetTailleX(Bonus bonus,unsigned int tailleX);
+void BonusSetTailleY(Bonus bonus,unsigned int tailleY);
+void BonusSetEffetBoonus(Bonus bonus,EffetBonus effet);
+
+/**Constructeur du Bonus*/
+void BonusConstructeur(Bonus *bonus, float positionX,float positionY,unsigned int tailleX,unsigned int tailleY,EffetBonus effet);
+/**Destructeur du Bonus*/
+void BonusDestructeur(Bonus *bonus);
+
+
+/**Fonction qui teste si une moto rentre en collision avec une Moto*/
+char BonusTestCollisionMoto(Moto *moto,Bonus* bonus);
+/**Fonction qui teste si un bonus est en Collision avec un Mur*/
+char BonusTestCollisionMurs(Grid *grid,Bonus* bonus);
+/**fonction qui place des bonus sur la grille*/
+void PlaceBonus(Grid* grid);
+
+
+#endif // BONUS_H_INCLUDED
