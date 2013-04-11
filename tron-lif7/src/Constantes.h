d46f088657b4fe4a77b18f8b1bfe38fe05bb4509
Antoine CARON <Slashgear@users.noreply.github.com>
2013-04-11 18:46:46 +0000

    Ajout des Joysticks mais non effectifs à revoir avec Matthei et une Mannette. Avancement de Bonus.

--- tron-lif7/src/Constantes.h
+++ tron-lif7/src/Constantes.h
@@ -5,5 +5,5 @@
 #define _Duree_Vie_Mur 350/_Nombre_de_Joueur
 #define _Vitesse_Initiale 3
 #define _Acceleration 0.02f
-
+#define _Nombre_de_Bonus 4
 #endif
--- tron-lif7/src/Jeu.h
+++ tron-lif7/src/Jeu.h
@@ -5,6 +5,7 @@
 #include "Joueur.h"
 #include "Grid.h"
 #include "Constantes.h"
+#include "Bonus.h"
 
 
 
@@ -12,20 +13,25 @@ typedef struct
 {
 	Grid grille;
 	Joueur mesJoueurs[_Nombre_de_Joueur];
+	/*Bonus tableauDeBonus[_Nombre_de_Bonus];*/
 } Jeu;
 
 /**assesseur de grille*/
 Grid* JeuGetGrille(Jeu* jeu);
 /**assesseur de mesjoueurs*/
 Joueur* JeuGetIemeJoueurs(Jeu* jeu,int i);
+/**assesseur du ieme  Bonus*/
+Bonus* JeuGetIemeBonus(Jeu* jeu,int i);
 
 /**mutateur de grille*/
 void JeuSetGrille(Jeu* jeu,Grid* grille);
 /**mutateur de mesjoueurs*/
 void JeuSetIemeJoueurs(Jeu* jeu,Joueur* mesJoueurs,int i);
+/**mutateur du ieme bonus*/
+void JeuSetIemeBonus(Jeu* jeu,Bonus* bonus,int i);
 
 /**constructeur du jeu*/
-void JeuConstructeur(Jeu* jeu, Grid* grille, Joueur* mesJoueurs[_Nombre_de_Joueur]);
+void JeuConstructeur(Jeu* jeu, Grid* grille, Joueur* mesJoueurs[_Nombre_de_Joueur]/*,Bonus* tableauDeBonus[_Nombre_de_Bonus]*/);
 /**destructeur du jeu*/
 void JeuDestructeur(Jeu* jeu);
 /**Boucle d'évolutions du jeu*/
