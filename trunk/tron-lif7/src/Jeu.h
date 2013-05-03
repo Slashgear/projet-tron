3b00e42655be4bd8cfcf9a874505aa5dfc29ea18
matthieu.billet3 <matthieu.billet3@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-05-03 17:34:42 +0000

    affichage grillaAnalyse (en cours de débugage)

--- trunk/tron-lif7/src/Jeu.h
+++ trunk/tron-lif7/src/Jeu.h
@@ -64,6 +64,8 @@ void JeuActionClavier(Joueur* joueur, Direction direction,Grid* grille);
 void JeuActionneBonus(Joueur*Joueur);
 /**procedure qui gère les mouvements d'une IA*/
 void JeuGereIA(Joueur* joueur,Jeu* jeu);
+/**procédure qui affiche le tableau d'analyse du la grille*/
+void AfficheGrilleAnalyse(short int (*grilleAnalyse)[_Taille_Y_Grille/_Precision_Analyse_IA][_Taille_X_Grille/_Precision_Analyse_IA]);
 
 /**Fonction qui teste la collision entre deux objets carrés (autrement appelé bounding box)*/
 char testCollisionGenerique(float objet1[4],float objet2[4]);
