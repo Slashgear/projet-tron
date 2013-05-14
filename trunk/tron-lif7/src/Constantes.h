47b35d4e184a1f2ac20ce0f96fd545133705d753
antoine395.caron@laposte.net <antoine395.caron@laposte.net@250f65ee-fa10-d43d-d320-e254c0aa752b>
2013-05-14 08:45:12 +0000

    ajout d'une fonction qui affiche les commentaires

--- trunk/tron-lif7/src/Constantes.h
+++ trunk/tron-lif7/src/Constantes.h
@@ -1,9 +1,11 @@
 #ifndef CONSTANTES_H_INCLUDED
 #define CONSTANTES_H_INCLUDED
 
-#define _Nombre_de_Joueur 8
+#define _Nombre_de_Joueur 2
 #define _Nombre_de_Manette 0
-#define _Nombre_IA 6
+#define _Nombre_IA 1
+#define _Nombre_de_Textes 2
+#define _Nombre_Images_Interface 2
 
 #define _Position_X_Grille 5
 #define _Position_Y_Grille 5
@@ -13,7 +15,7 @@
 #define _Precision_Analyse_IA 10
 
 #define _Duree_Vie_Mur 150
-#define _Vitesse_Initiale 7
+#define _Vitesse_Initiale 12
 #define _Acceleration 0.02f
 
 #define _Nombre_de_Bonus 4
